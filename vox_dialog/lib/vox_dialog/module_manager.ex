87c603f64368ff60034af15cd6ff30395b3b5e1b
BBC-Esq <bbc@chintellalaw.com>
2025-06-07 09:27:28 -0400

    Update module_manager.ex

--- vox_dialog/lib/vox_dialog/module_manager.ex
+++ vox_dialog/lib/vox_dialog/module_manager.ex
@@ -50,59 +50,74 @@ defmodule VoxDialog.ModuleManager do
     {:ok, state}
   end
   
-@impl true
-def handle_call({:load_module, module_id, opts}, _from, state) do
-  case get_module_implementation(module_id) do
-    nil ->
-      {:reply, {:error, :module_not_found}, state}
-    
-    module ->
-      # Add backend configuration for STT module
-      enhanced_opts = case module_id do
-        "stt" ->
-          backend_type = Map.get(opts, :backend_type)
-          if backend_type do
-            Map.put(opts, :backend_type, String.to_atom(backend_type))
-          else
-            opts
-          end
-        _ ->
-          opts
-      end
+  @impl true
+  def handle_call({:load_module, module_id, opts}, _from, state) do
+    case get_module_implementation(module_id) do
+      nil ->
+        {:reply, {:error, :module_not_found}, state}
       
-      case module.initialize(enhanced_opts) do
-        {:ok, module_state} ->
-          Logger.info("Loaded module: #{module_id}")
-          
-          new_state = state
-          |> put_in([:loaded_modules, module_id], module)
-          |> put_in([:module_states, module_id], module_state)
-          
-          # Broadcast module loaded event with backend info
-          event_data = case module_id do
-            "stt" ->
-              case VoxDialog.SpeechRecognition.get_backend_info() do
-                {:ok, backend_info} -> %{module_id: module_id, backend: backend_info}
-                _ -> %{module_id: module_id}
-              end
-            _ ->
-              %{module_id: module_id}
-          end
-          
-          Phoenix.PubSub.broadcast(
-            VoxDialog.PubSub,
-            "module_status",
-            {:module_loaded, event_data}
-          )
-          
-          {:reply, :ok, new_state}
+      module ->
+        # Add backend configuration for STT module
+        enhanced_opts = case module_id do
+          "stt" ->
+            # Handle backend_type parameter
+            backend_type = case Map.get(opts, :backend_type) do
+              backend when is_binary(backend) -> String.to_atom(backend)
+              backend when is_atom(backend) -> backend
+              _ -> nil
+            end
+            
+            if backend_type do
+              Map.put(opts, :backend_type, backend_type)
+            else
+              opts
+            end
+          _ ->
+            opts
+        end
         
-        {:error, reason} ->
-          Logger.error("Failed to load module #{module_id}: #{inspect(reason)}")
-          {:reply, {:error, reason}, state}
-      end
+        case module.initialize(enhanced_opts) do
+          {:ok, module_state} ->
+            Logger.info("Loaded module: #{module_id}")
+            
+            new_state = state
+            |> put_in([:loaded_modules, module_id], module)
+            |> put_in([:module_states, module_id], module_state)
+            
+            # Broadcast module loaded event with backend info
+            event_data = case module_id do
+              "stt" ->
+                case VoxDialog.SpeechRecognition.get_backend_info() do
+                  {:ok, backend_info} -> 
+                    %{
+                      module_id: module_id, 
+                      backend: backend_info,
+                      backend_type: Map.get(enhanced_opts, :backend_type)
+                    }
+                  _ -> 
+                    %{
+                      module_id: module_id,
+                      backend_type: Map.get(enhanced_opts, :backend_type)
+                    }
+                end
+              _ ->
+                %{module_id: module_id}
+            end
+            
+            Phoenix.PubSub.broadcast(
+              VoxDialog.PubSub,
+              "module_status",
+              {:module_loaded, event_data}
+            )
+            
+            {:reply, :ok, new_state}
+          
+          {:error, reason} ->
+            Logger.error("Failed to load module #{module_id}: #{inspect(reason)}")
+            {:reply, {:error, reason}, state}
+        end
+    end
   end
-end
   
   @impl true
   def handle_call({:unload_module, module_id}, _from, state) do
@@ -121,7 +136,6 @@ end
         |> update_in([:module_states], &Map.delete(&1, module_id))
         |> update_in([:module_pipes], &remove_module_pipes(&1, module_id))
         
-        # Broadcast module unloaded event
         Phoenix.PubSub.broadcast(
           VoxDialog.PubSub,
           "module_status",
@@ -162,13 +176,11 @@ end
           {:ok, output, new_module_state} ->
             new_state = put_in(state, [:module_states, module_id], new_module_state)
             
-            # Check if this module is piped to another
             case Map.get(state.module_pipes, module_id) do
               nil ->
                 {:reply, {:ok, output}, new_state}
               
               target_module_id ->
-                # Forward output to target module
                 handle_call({:send_to_module, target_module_id, output}, self(), new_state)
             end
           
@@ -180,7 +192,6 @@ end
   
   @impl true
   def handle_call({:pipe_modules, source_id, target_id}, _from, state) do
-    # Verify both modules are loaded
     source_module = Map.get(state.loaded_modules, source_id)
     target_module = Map.get(state.loaded_modules, target_id)
     
